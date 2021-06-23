e5666be11767a257cd2aadd2a6abc8b9e2df918e
wijayam <mwijaya3@hotmail.com>
2021-06-23 16:41:25 -0700

    modify init file

--- tap_s3_csv/__init__.py
+++ tap_s3_csv/__init__.py
@@ -83,8 +83,12 @@ def main():
 
     if args.discover:
         do_discover(args.config)
-    elif args.properties:
-        do_sync(config, args.properties, args.state)
+    else:
+        if args.catalog:
+            catalog = args.catalog
+        else:
+            catalog = do_discover(args.config)
+        do_sync(config, args.catalog, args.state)
 
 
 if __name__ == '__main__':
